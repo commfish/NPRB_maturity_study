17f39002aed7ba7e2c9c832bc992362c928c9ecd
Ben Williams <bcwilliams2@alaska.edu>
2018-12-10 18:31:45 -0900

    bw edits

--- code/objective_II.R
+++ code/objective_II.R
@@ -1,107 +1,61 @@
-#notes---
-#author: Sara E Miller 
-#contact: sara.miller@alaska.gov; 907-465-4245
-#Last edited: December, 2018
+# notes---
+# author: Sara E Miller 
+# contact: sara.miller@alaska.gov; 907-465-4245
+# Last edited: December, 2018
 
 # load ----
-is_installed <- function(mypkg){ is.element(mypkg, installed.packages()[,1])}
-
-load_or_install <- function(package_names){
-  for(package_name in package_names){
-    if(!is_installed(package_name)){install.packages(package_name,repos="http://lib.stat.cmu.edu/R/CRAN")}
-    library(package_name,character.only=TRUE,quietly=TRUE,verbose=FALSE)
-  }}
-
-load_or_install(c("extrafont",
-                  "tidyverse",
-                  "dgof",
-                  "readxl",
-                  "Matching",
-                  "devtools",
-                  "FNGr",
-                  "cowplot"))
-
-tickr <- function(
-  data, # dataframe
-  var, # column of interest
-  to # break point definition 
-){
-  
-  VAR <- enquo(var) # makes VAR a dynamic variable
-  
-  data %>% 
-    distinct(!!VAR) %>%
-    #    ungroup(!!VAR) %>% 
-    mutate(labels = ifelse(!!VAR %in% seq(to * round(min(!!VAR) / to), max(!!VAR), to),
-                           !!VAR, "")) %>%
-    dplyr::select(breaks = UQ(VAR), labels)
-}
-
+source("code/helper.r")
 
 # load data ---- 
-data <- read.csv("data/data_11_26_2018.csv", check.names = FALSE) 
+read.csv("data/data_11_26_2018.csv", check.names = FALSE) %>% 
+  filter(!(maturation_status_histology %in% c("", "NA", "no slide", "no score", "4-1", "3-4", "2-3", "1-2")))  -> data_clean
 
-#clean data ----
-data %>% filter(maturation_status_histology != "") %>% 
-  filter(maturation_status_histology != "NA") %>% 
-  filter(maturation_status_histology != "no slide") %>% 
-  filter(maturation_status_histology != "no score") %>% 
-  filter(maturation_status_histology != "4-1") %>% 
-  filter(maturation_status_histology != "3-4") %>%
-  filter(maturation_status_histology != "2-3") %>% 
-  filter(maturation_status_histology != "1-2")  -> data_clean
-
-data.clean %>%
-  mutate (age = as.numeric(age)) %>%
+data_clean %>%
   group_by(age, maturation_status_histology) %>%
   summarise(count = n()) -> table1 
 
-data.clean %>%
+data_clean %>%
   group_by(maturity_state_GSI, maturation_status_histology) %>%
   summarise(count = n()) -> table2 
 
 
-data.clean %>%
+data_clean %>%
   group_by(maturity_state_field, maturation_status_histology) %>%
   summarise(count = n()) -> table3 
 
-data %>% filter(sex_histology != "") %>% 
-  filter(sex_histology != "NA") %>% 
-  filter(sex_histology != "no slide") -> data_clean
+data %>% 
+  filter(!(sex_histology %in% c("", "NA", "no slide"))) -> data_clean
 
 data_clean %>%
-  mutate (date = as.Date(catch_date, format='%m/%d/%Y')) %>%
+  mutate (date = mdy(catch_date)) %>%
   group_by(maturity_state_GSI, date, sex_histology) %>%
   summarise(count = n(), 
             mean = mean (GSI)) -> table4 
 
-#figures 
-loadfonts(device="win")
-windowsFonts(Times=windowsFont("TT Times New Roman"))
-theme_set(theme_bw(base_size=12, base_family='Times New Roman') +
-            theme(panel.grid.major = element_blank(), 
-                  panel.grid.minor = element_blank(),
-                  plot.title = element_text(family='Times New Roman', hjust = 0.5, size=12)))
-tickr_length <- data.frame(length_millimeters = 200:1000)
-axisf <- tickr(tickr_length, length_millimeters, 100)
+#figures ----
+
 
 #bubble plot of age versus histology
 bb <- c(201,100,50,20,10,5) # define breaks.
 ll <- c("200+","100","50","20","10", "5") # labels.
-p<-ggplot(table1)+ geom_point(aes(x = age, y = maturation_status_histology, size = count),shape=16, alpha=0.80) +
-  labs(x="Age", y = "Maturation Status (histology)") +  scale_size_continuous(name = "",
-                                                                                breaks = bb,
-                                                                                limits = c(0, 250),
-                                                                                labels = ll,
-                                                                                range = c(0, 10))
+
+ggplot(table1) + 
+  geom_point(aes(age, maturation_status_histology, size = count), shape=16, alpha=0.80) +
+  labs(x="Age", y = "Maturation Status (histology)\n") +  
+  scale_size_continuous(name = "",
+                        breaks = bb,
+                        limits = c(0, 250),
+                        labels = ll,
+                        range = c(0, 10))
+
 ggsave(filename = 'figs/bubbleplot.png', dpi =200, width=6, height=8, units = "in")
 
 
 
-#confidence intervals for a difference in proportions (package PropCIs) (see package for refs)
-#diffscoreci(x1, n1, x2, n2, conf.level)
-#wald2ci(x1, n1, x2, n2, conf.level, adjust)
-#prop.test (MASS package)
+# confidence intervals for a difference in proportions (package PropCIs) (see package for refs)
+# diffscoreci(x1, n1, x2, n2, conf.level)
+# wald2ci(x1, n1, x2, n2, conf.level, adjust)
+# prop.test (MASS package)
 prop.test(x=c(122,99), n=c(600,400), correct=F)
 
 diffscoreci(7, 21, 13, 17, conf.level=0.95)
