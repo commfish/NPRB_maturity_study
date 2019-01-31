3ce69eda6cf814197314aa6f050278a99482da30
Miller <sara.miller@dfg.alaska.local>
2019-01-30 17:06:07 -0900

    update data to include GSI by number and not percentage; calculate diff between histology and GSI

--- code/objective_III.R
+++ code/objective_III.R
@@ -50,28 +50,83 @@ tickr <- function(
 data <- read.csv("data/data_11_26_2018.csv", check.names = FALSE) 
 
 #clean data ----
-data %>% filter(!(maturation_status_histology %in% c("", NA, "no slide", "no score", "4-1", "3-4", "2-3", "1-2"))) %>%
-         filter(!(age %in% c(NA)))  -> data_clean
-
-data_clean %>%
-  mutate (age = as.numeric(age)) %>%
-  group_by(age, maturation_status_histology) %>%
-  summarise(count = n()) -> table1 
 
 data %>% filter(!(maturation_status_histology %in% c("", NA, "no slide", "no score", "4-1", "3-4", "2-3", "1-2")))-> data_clean
-
-data_clean %>%
-  group_by(maturity_state_GSI, maturation_status_histology) %>%
-  summarise(count = n()) -> table2 
 
 data_clean %>%
   group_by(maturity_state_field, maturation_status_histology) %>%
   summarise(count = n()) -> table3 
 
-data %>% filter(!(sex_histology %in% c("", NA, "no slide")))  -> data_clean
 
-maturity_state_field
+#cohen kappa evaluation (macro and histology)
+data %>% filter(!(maturation_status_histology %in% c("", NA, "no slide", "no score", "4-1", "3-4", "2-3", "1-2", "7", "6"))) %>%
+dplyr::select(maturation_status_histology = maturation_status_histology,
+              maturity_state_field = maturity_state_field) -> data_clean
+data_clean %>%
+  group_by(maturity_state_field, maturation_status_histology) %>%
+  summarise(count = n()) -> table3a 
+cohen.kappa(data_clean)
 
+
+#Two proportions z-test with continuity correction
+#The function returns:
+  #the value of Pearson’s chi-squared test statistic.
+  #a p-value
+  #a 95% confidence intervals
+  #an estimated probability of success 
+#macroscopic versus histology
+data %>% filter(!(maturation_status_histology %in% c("", NA, "no slide", "no score", "4-1", "3-4", "2-3", "1-2", "7", "6"))) %>%
+  mutate(maturation_status_histology = as.numeric(maturation_status_histology),
+                      maturity_state_field = as.numeric(maturity_state_field)) %>%
+  mutate(status_h = ifelse(maturation_status_histology>1, 'mature','immature'),
+         status_m = ifelse(maturity_state_field>1, 'mature','immature')) -> data_clean
+
+data_clean %>%
+  dplyr::select(status_h) %>%
+  group_by(status_h) %>%
+  summarise(count = n()) -> tablex  
+
+data_clean %>%
+  dplyr::select(status_m) %>%
+  group_by(status_m) %>%
+  summarise(count = n()) -> tabley
+
+res <- prop.test(x = c(264, 141), n = c(726, 726),  correct = FALSE)
+
+data %>% filter(!(maturation_status_histology %in% c("", NA, "no slide", "no score", "4-1", "3-4", "2-3", "1-2", "7", "6"))) %>%
+  mutate(maturation_status_histology = as.numeric(maturation_status_histology),
+         maturity_state_field = as.numeric(maturity_state_field)) %>%
+  mutate(status_h = ifelse(maturation_status_histology>1, 'mature','immature'),
+         status_m = ifelse(maturity_state_field>1, 'mature','immature')) %>%
+dplyr::select(status_h, status_m) -> data_clean
+
+cohen.kappa(data_clean)
+#GSI versus histology
+data %>% filter(!(maturation_status_histology %in% c("", NA, "no slide", "no score", "4-1", "3-4", "2-3", "1-2", "7", "6"))) %>%
+  mutate(maturation_status_histology = as.numeric(maturation_status_histology),
+                      GSI = as.numeric(GSI)) %>%
+  mutate(status_h = ifelse(maturation_status_histology>1, 'mature','immature'),
+         status_gsi = ifelse(GSI>0.0499, 'mature','immature')) -> data_clean
+
+data_clean %>%
+  dplyr::select(status_h) %>%
+  group_by(status_h) %>%
+  summarise(count = n()) -> tablex  
+
+data_clean %>%
+  dplyr::select(status_gsi) %>%
+  group_by(status_gsi) %>%
+  summarise(count = n()) -> tabley
+
+res <- prop.test(x = c(264, 709), n = c(726, 726),  correct = FALSE)
+data %>% filter(!(maturation_status_histology %in% c("", NA, "no slide", "no score", "4-1", "3-4", "2-3", "1-2", "7", "6"))) %>%
+  mutate(maturation_status_histology = as.numeric(maturation_status_histology),
+         GSI = as.numeric(GSI)) %>%
+  mutate(status_h = ifelse(maturation_status_histology>1, 'mature','immature'),
+         status_gsi = ifelse(GSI>0.0499, 'mature','immature'))%>%
+  dplyr::select(status_h, status_gsi) -> data_clean
+
+cohen.kappa(data_clean)
 #figures 
 loadfonts(device="win")
 windowsFonts(Times=windowsFont("TT Times New Roman"))
@@ -87,39 +142,10 @@ bb <- c(201,100,50,20,10,5) # define breaks.
 ll <- c("200+","100","50","20","10", "5") # labels.
 p<-ggplot(table1)+ geom_point(aes(x = age, y = maturation_status_histology, size = count),shape=16, alpha=0.80) +
   labs(x="Age", y = "Maturation Stage (histology)") +  scale_size_continuous(name = "",
-                                                                                breaks = bb,
-                                                                                limits = c(0, 250),
-                                                                                labels = ll,
-                                                                                range = c(0, 10))
+                                                                             breaks = bb,
+                                                                             limits = c(0, 250),
+                                                                             labels = ll,
+                                                                             range = c(0, 10))
 ggsave(filename = 'figs/bubbleplot.png', dpi =200, width=6, height=8, units = "in")
 
-#cohen kappa evaluation
-data %>% filter(!(maturation_status_histology %in% c("", NA, "no slide", "no score", "4-1", "3-4", "2-3", "1-2", "7", "6"))) %>%
-dplyr::select(maturation_status_histology = maturation_status_histology,
-              maturity_state_field = maturity_state_field) -> data_clean
 
-cohen.kappa(data_clean)
-
-#Two proportions z-test with continuity correction
-#The function returns:
-  #the value of Pearson’s chi-squared test statistic.
-  #a p-value
-  #a 95% confidence intervals
-  #an estimated probability of success 
-
-data_clean %>% mutate(maturation_status_histology = as.numeric(maturation_status_histology),
-                      maturity_state_field = as.numeric(maturity_state_field)) %>%
-  mutate(status_h = ifelse(maturation_status_histology>1, 'mature','immature'),
-         status_m = ifelse(maturity_state_field>1, 'mature','immature')) -> data_clean
-
-data_clean %>%
-  dplyr::select(status_h) %>%
-  group_by(status_h) %>%
-  summarise(count = n()) -> tablex  
-
-data_clean %>%
-  dplyr::select(status_m) %>%
-  group_by(status_m) %>%
-  summarise(count = n()) -> tabley
-
-res <- prop.test(x = c(264, 141), n = c(727, 727),  correct = FALSE)
