ff2086cc31502aa0c5252f13b441a9559b682447
Miller <sara.miller@dfg.alaska.local>
2019-05-09 15:54:19 -0800

    check for duplicates and merge data

--- code/objective_I.R
+++ code/objective_I.R
@@ -19,3 +19,20 @@ asintrans <- function(p) {asin(sqrt(p))} # arctransformation function
 # load data ---- 
 data <- read.csv("data/data_11_26_2018.csv", check.names = FALSE) 
 increment <- read.csv("data/increment_measurements.csv", check.names = FALSE) 
+
+#data clean ----
+data %>% 
+  filter(!(maturation_status_histology %in% c("", NA, "no slide", "no score", "4-1", "3-4", "2-3", "1-2")))-> data_clean
+
+# check for duplicate image names in the increment file
+increment %>% 
+  group_by(image_name) %>% 
+  filter(n()>1) %>% 
+  summarize(n=n())-> x
+
+# match increments to awl data
+merge1<-merge(increment, data_clean, all.y=T)
+merge1 %>% 
+  filter(!(Increment1 %in% c("", NA)))-> clean_dataset #n=573 useable samples
+
+
