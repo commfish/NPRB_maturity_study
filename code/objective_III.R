b962bd45f2544aca64e8008113f97408f6a87b62
Miller <sara.miller@dfg.alaska.local>
2020-05-05 11:18:41 -0800

    commit

--- code/objective_III.R
+++ code/objective_III.R
@@ -1,6 +1,6 @@
 #author: Sara E Miller 
 #contact: sara.miller@alaska.gov; 907-465-4245
-#Last edited: March, 2020
+#Last edited: May, 2020
 
 # load ----
 #font_import() #only do this one time - it takes a while
@@ -18,6 +18,7 @@ library(cowplot)
 library(psych)
 library(broom)
 library(lubridate)
+library(plyr)
 theme_set(theme_sleek())
 
 # load data ---- 
@@ -26,42 +27,52 @@ spawn <- read.csv("data/spawn_timing_Sitka.csv", check.names = FALSE)
 
 #clean data ----
 data %>% filter(!(maturation_status_histology %in% c("", NA, "no slide", "no score", "4-1", "3-4", "2-3", "1-2"))) %>%
-         filter(!(sex_histology %in% c("Male"))) -> data_clean
-write.csv(data_clean, "data/cpue_new_test.csv") #dataset for GSI/histology
-data_clean %>%
-  group_by(maturity_state_field, maturation_status_histology) %>%
-  summarise(count = n()) -> table3 
-data_clean %>%
-  group_by(age, maturation_status_histology) %>%
-  summarise(count = n()) -> table4 
-write.csv(data_clean, "data/cpue_new_test.csv") 
+         filter(!(sex_histology %in% c("Male"))) %>%
+         filter(!(sample_no %in% c("743", "629", "334", "289"))) -> data_clean
+write.csv(data_clean, "data/cpue_new_test.csv") #dataset for GSI/histology by age
 
-#Macroscopic versus Histology (all stages)
-#cohen kappa evaluation 
+# Macroscopic versus Histology (all stages)
+# cohen kappa evaluation 
 data %>% filter(!(maturation_status_histology %in% c("", NA, "no slide", "no score", "4-1", "3-4", "2-3", "1-2", "7", "6"))) %>%
   filter(!(sex_histology %in% c("Male"))) %>%
-  dplyr::select(maturation_status_histology,maturity_state_field) -> data_clean
+  filter(!(sample_no %in% c("743", "629", "334", "289"))) %>%
+  dplyr::mutate(maturity_histology = ifelse(maturation_status_histology == 1, "immature" , "mature")) -> data_clean
 data_clean %>%
+  dplyr::select(maturation_status_histology,maturity_state_field) %>%
   group_by(maturity_state_field, maturation_status_histology) %>%
   summarise(count = n()) -> table1
-cohen.kappa(data_clean) # all stages (psych package)
+data_clean %>%
+  dplyr::select(maturation_status_histology,maturity_state_field) %>%
+  cohen.kappa(.) # all stages (psych package)
 
-#Macroscopic versus Histology (mature/immature)
-#cohen kappa evaluation 
+# Histology by age
 data %>% filter(!(maturation_status_histology %in% c("", NA, "no slide", "no score", "4-1", "3-4", "2-3", "1-2", "7", "6"))) %>%
   filter(!(sex_histology %in% c("Male"))) %>%
+  filter(!(sample_no %in% c("743", "629", "334", "289"))) %>%
+  dplyr::mutate(maturity_histology = ifelse(maturation_status_histology == 1, "immature" , "mature")) -> data_clean
+data_clean %>%
+  dplyr::select(maturity_histology,age) %>%
+  group_by(maturity_histology, age) %>%
+  summarise(count = n()) -> table2
+
+# Macroscopic versus Histology (mature/immature)
+# cohen kappa evaluation 
+data %>% filter(!(maturation_status_histology %in% c("", NA, "no slide", "no score", "4-1", "3-4", "2-3", "1-2", "7", "6"))) %>%
+  filter(!(sex_histology %in% c("Male"))) %>%
+  filter(!(sample_no %in% c("743", "629", "334", "289"))) %>%
   dplyr::mutate(maturity_histology = ifelse(maturation_status_histology == 1, "immature" , "mature")) %>%
   dplyr::mutate(maturity_field = ifelse(maturity_state_field == 1, "immature" , "mature")) %>%
   dplyr::select(maturity_histology, maturity_field) -> data_clean
 
 data_clean %>%
   group_by(maturity_histology, maturity_field) %>%
-  summarise(count = n()) -> table2 
+  summarise(count = n()) -> table3 
 cohen.kappa(data_clean) # all stages
 
-#GSI versus histology
+# GSI versus histology
 data %>% filter(!(maturation_status_histology %in% c("", NA, "no slide", "no score", "4-1", "3-4", "2-3", "1-2", "7", "6"))) %>%
   filter(!(sex_histology %in% c("Male"))) %>%
+  filter(!(sample_no %in% c("743", "629", "334", "289"))) %>%
   dplyr::mutate(maturity_histology = ifelse(maturation_status_histology == 1, "immature" , "mature")) %>%
   mutate(GSI=as.numeric(GSI)) %>%
   dplyr::mutate(maturity_GSI = ifelse(GSI > 0.05, "mature" , "immature")) %>%
@@ -70,11 +81,12 @@ cohen.kappa(data_clean) # all stages
 data_clean %>%
   dplyr::select(maturity_histology, maturity_GSI) %>%
   group_by(maturity_histology, maturity_GSI) %>%
-  summarise(count = n()) -> table3 
+  summarise(count = n()) -> table4 
 
-# filter GSI = 0
+## filter GSI = 0
 data %>% filter(!(maturation_status_histology %in% c("", NA, "no slide", "no score", "4-1", "3-4", "2-3", "1-2", "7", "6"))) %>%
   filter(!(sex_histology %in% c("Male"))) %>%
+  filter(!(sample_no %in% c("743", "629", "334", "289"))) %>%
   dplyr::mutate(maturity_histology = ifelse(maturation_status_histology == 1, "immature" , "mature")) %>%
   mutate(GSI=as.numeric(GSI)) %>%
   dplyr::mutate(maturity_GSI = ifelse(GSI > 0.05, "mature" , "immature")) %>%
@@ -85,11 +97,12 @@ data_clean %>%
   group_by(maturity_histology, sex_histology) %>%
   summarise(n = n(),
             median = median(GSI, na.rm=T), mean =mean(GSI),sd=sd(GSI),se=sd(GSI)/sqrt(n()), min = min(GSI), max =max(GSI))%>%
-  as.data.frame() -> table4  #average, se, min, max of GSI
+  as.data.frame() -> table5  #average, se, min, max of GSI
 
-#Boxplot of GSI cutoff by sex
+# boxplot of GSI cutoff by sex
 data %>% filter(!(maturation_status_histology %in% c("", NA, "no slide", "no score", "4-1", "3-4", "2-3", "1-2", "7", "6"))) %>%
   filter(!(sex_histology %in% c("Male"))) %>%
+  filter(!(sample_no %in% c("743", "629", "334", "289"))) %>%
   dplyr::mutate(maturity_histology = ifelse(maturation_status_histology == 1, "immature" , "mature")) %>%
   mutate(GSI=as.numeric(GSI)) %>%
   dplyr::mutate(maturity_GSI = ifelse(GSI > 0.05, "mature" , "immature")) %>%
@@ -100,15 +113,17 @@ ggplot(., aes(maturity_histology, GSI, colour = maturity_histology)) +
   labs(x="", y = "GSI") + theme(legend.position="none")+scale_colour_grey(start = .1, end = .1)
 ggsave(filename = 'figs/gsi_boxplots.png', dpi =200, width=8, height=6, units = "in")
 
-#regression (females)
+# GSI regression (females)
 data %>% filter(!(maturation_status_histology %in% c("", NA, "no slide", "no score", "4-1", "3-4", "2-3", "1-2", "7", "6"))) %>%
   filter(!(sex_histology %in% c("Male"))) %>%
+  filter(!(sample_no %in% c("743", "629", "334", "289"))) %>%
   dplyr::mutate(maturity_histology = ifelse(maturation_status_histology == 1, "0" , "1")) %>%
   mutate(GSI=as.numeric(GSI)) %>%
   dplyr::mutate(maturity_GSI = ifelse(GSI > 0.05, "mature" , "immature")) %>%
   filter(!(GSI %in% c(0))) %>%
   arrange(maturity_histology, GSI)-> data_clean
 
+## cut-off of 1.4%
 data_clean %>%
   filter(!(maturity_histology %in% c(0))) %>%
   mutate(value_I = ifelse(GSI>0.014,1,0)) %>%
@@ -128,6 +143,7 @@ data_clean %>%
             sumv=sum(value_I),
             min=min(GSI))-> cutoff_1.4_immature
 
+## cut-off of 0.6%
 data_clean %>%
   filter(!(maturity_histology %in% c(0))) %>%
   mutate(value_I = ifelse(GSI>0.006,1,0)) %>%
@@ -164,7 +180,9 @@ lm_out %>%
   augment(A2) %>% 
   mutate(gsi = exp(log_gsi), 
          length = exp(log_length), 
-         fit = exp(.fitted)) -> data2 
+         fit = exp(.fitted)) -> data2
+
+# GSI frequency and cut-off plot
 ggplot() +
   geom_point(data=data1, aes(x = length, y = gsi), color ="grey50") + 
   geom_point(data=data2, aes(length, gsi), color = "black")+ 
@@ -180,6 +198,7 @@ ggplot() +
 
 data %>% filter(!(maturation_status_histology %in% c("", NA, "no slide", "no score", "4-1", "3-4", "2-3", "1-2", "7", "6"))) %>%
   filter(!(sex_histology %in% c("Male"))) %>%
+  filter(!(sample_no %in% c("743", "629", "334", "289"))) %>%
   dplyr::mutate(maturity_histology = ifelse(maturation_status_histology == 1, "immature" , "mature")) %>%
   mutate(GSI=as.numeric(GSI))-> data_clean
 
@@ -229,23 +248,3 @@ merge %>%
   scale_color_manual(values=c("#999999", "black")) +theme(legend.title=element_blank(), legend.position=c(.15,.85)) +
   scale_fill_manual(values=c("#999999", "black" )) -> plot1
 
-#Two proportions z-test with continuity correction
-#The function returns:
-#the value of Pearson’s chi-squared test statistic.
-#a p-value
-#a 95% confidence intervals
-#an estimated probability of success 
-
-#Pearson's X^2
-#data %>% filter(!(maturation_status_histology %in% c("", NA, "no slide", "no score", "4-1", "3-4", "2-3", "1-2", "7", "6"))) %>%
-#  filter(!(sex_histology %in% c("Male"))) %>%
-#  dplyr::select(maturity_state_histology) %>%
-#  group_by(maturity_state_histology) %>%
-#  summarise(count = n()) -> tablex  
-
-#data_clean %>%
-#  dplyr::select(maturity_state_Hjort) %>%
-#  group_by(maturity_state_Hjort) %>%
-#  summarise(count = n()) -> tabley
-
-#res <- prop.test(x = c(337, 335), n = c(726, 726),  correct = FALSE) #immature
