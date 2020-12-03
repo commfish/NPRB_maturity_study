0fb8f917771da55117a3338b39ab87a1e5e5d788
Miller <sara.miller@dfg.alaska.local>
2020-12-03 13:15:00 -0900

    commit

--- code/objective_I.R
+++ code/objective_I.R
@@ -30,7 +30,7 @@ library("ggpubr")
 library("devtools")
 devtools::install_github("commfish/fngr")
 library(fngr)
-theme_set(theme_report(base_size = 12))
+theme_set(theme_report(base_size = 16))
 
 # load data ---- 
 data <- read.csv("data/data_11_26_2018.csv", check.names = FALSE) 
@@ -525,69 +525,69 @@ ggsave("figs/hist_obj1.png", dpi = 500, height = 8, width =10, units = "in")
 merge %>% 
   mutate(age=as.numeric(age))%>% 
   ggplot(data=., aes(x = age, y = anu_adj, color = maturity, shape = maturity)) +
-  geom_jitter(size = 1) +
-  labs(x = "Age",y = "Outer increment measurement (mm)") + 
-  theme(legend.title=element_blank(), legend.position=c(.8,.9), legend.text=element_text(size=12)) +
+  geom_jitter(size = 3) +
+  labs(x = "Age",y = "Outer incremental measurement (mm)") + 
+  theme(legend.title=element_blank(), legend.position=c(.8,.9), legend.text=element_text(size=17)) +
   scale_color_manual(values=c("#999999", "black")) +
   scale_fill_manual(values=c("#999999", "black" )) +
   scale_shape_manual(values=c(8, 16)) +
   scale_x_continuous(breaks = c(2, 3, 4, 5, 6), limits = c(1.5,6.5)) +
-  annotate("text",x = 1.5, y=2, label="A)", family="Times" ,size=4) +
+  annotate("text",x = 1.6, y=2, label="A)", family="Times" ,size=6) +
   scale_y_continuous(breaks = c(0, 0.2, 0.4, 0.6, 0.8, 1.0, 1.2, 1.4, 1.6, 1.8, 2.0), limits = c(0, 2.0))-> plot1
 
 merge %>%
   as.data.frame()%>% 
   mutate(age=as.factor(age)) %>% 
-  ggplot(data=.,aes(x = age, y = anu_adj)) + labs(x = "Age",y = "Outer increment measurement (mm)") + 
+  ggplot(data=.,aes(x = age, y = anu_adj)) + labs(x = "Age",y = "Outer incremental measurement (mm)") + 
   geom_boxplot(aes(fill = maturity)) +
-  scale_color_manual(values=c("#999999", "black")) + theme(legend.title=element_blank(), legend.position=c(.8,.9), legend.text=element_text(size=12)) +
+  scale_color_manual(values=c("#999999", "black")) + theme(legend.title=element_blank(), legend.position=c(.8,.9), legend.text=element_text(size=17)) +
   scale_fill_manual(values=c("#999999", "black" )) + 
   scale_y_continuous(breaks = c(0, 0.5, 1, 1.5, 2), limits = c(0, 2)) +
   #scale_x_continuous(breaks = c(2, 3, 4, 5, 6), limits = c(1.5,6.5)) +
-  annotate("text",x = 0.75, y=2, label="B)", family="Times" ,size=4)-> plot2
+  annotate("text",x = 0.75, y=2, label="B)", family="Times" ,size=6)-> plot2
 
 merge %>% 
   ggplot(data=., aes(x = age, y = radcap, color = maturity, shape = maturity)) +
-  geom_jitter(size = 1) +
+  geom_jitter(size = 2) +
   labs(x = "Age",y = "Cumulative growth (mm)") + 
-  theme(legend.title=element_blank(), legend.position=c(.8,.9), legend.text=element_text(size=12)) +
+  theme(legend.title=element_blank(), legend.position=c(.8,.9), legend.text=element_text(size=17)) +
   scale_color_manual(values=c("#999999", "black")) +
   scale_fill_manual(values=c("#999999", "black" )) +
   scale_shape_manual(values=c(8, 16)) +
-  annotate("text",x = 2, y=10, label="B)", family="Arial", colour="black", size=4) +
+  annotate("text",x = 2, y=10, label="B)", family="Arial", colour="black", size=6) +
   scale_y_continuous(breaks = c(0, 1,2,3,4,5,6,7,8,9,10), limits = c(0, 10))-> plot3
 
 cowplot::plot_grid(plot1, plot2,  align = "vh", nrow = 1, ncol=2)
-ggsave("figs/scatterplot_obj1.png", dpi = 500, height = 4, width = 8, units = "in")
+ggsave("figs/scatterplot_obj1.tiff", dpi = 500, height = 4, width = 8, units = "in")
 
 # Scatterplot Figures---- 
 ## growth up into the summer 
 merge %>% 
   mutate(age=as.numeric(age))%>% 
   ggplot(data=., aes(x = age, y = scale_length_prior, color = maturity, shape = maturity)) +
-  geom_jitter(size = 1) +
-  labs(x = "Age",y = "Scale measurement up to the last increment (mm)") + 
-  theme(legend.title=element_blank(), legend.position=c(.86,.2), legend.text=element_text(size=12)) +
+  geom_jitter(size = 3) +
+  labs(x = "Age",y = "Scale measurement (mm)") + 
+  theme(legend.title=element_blank(), legend.position=c(.80,.2), legend.text=element_text(size=17)) +
   scale_color_manual(values=c("#999999", "black")) +
   scale_fill_manual(values=c("#999999", "black" )) +
   scale_shape_manual(values=c(8, 16)) +
   scale_x_continuous(breaks = c(2, 3, 4, 5, 6), limits = c(1.5,6.5)) +
-  annotate("text",x = 1.5, y=6, label="A)", family="Times" ,size=4) +
+  annotate("text",x = 1.6, y=6, label="A)", family="Times" ,size=6) +
   scale_y_continuous(breaks = c(0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0), limits = c(0, 6.0)) -> plot1
 
 merge %>%
   as.data.frame()%>% 
   mutate(age=as.factor(age)) %>% 
-  ggplot(data=.,aes(x = age, y = scale_length_prior)) + labs(x = "Age",y = "Scale measurement up to the last increment (mm)") + 
+  ggplot(data=.,aes(x = age, y = scale_length_prior)) + labs(x = "Age",y = "Scale measurement (mm)") + 
   geom_boxplot(aes(fill = maturity)) +
-  scale_color_manual(values=c("#999999", "black")) + theme(legend.title=element_blank(), legend.position=c(.86,.2), legend.text=element_text(size=12)) +
+  scale_color_manual(values=c("#999999", "black")) + theme(legend.title=element_blank(), legend.position=c(.80,.2), legend.text=element_text(size=16)) +
   scale_fill_manual(values=c("#999999", "black" )) + 
   scale_y_continuous(breaks = c(0, 1, 2, 3, 4, 5, 6), limits = c(0, 6)) +
   #scale_x_continuous(breaks = c(2, 3, 4, 5, 6), limits = c(1.5,6.5)) +
-  annotate("text",x = 0.75, y=6, label="B)", family="Times" ,size=4) -> plot2
+  annotate("text",x = 0.75, y=6, label="B)", family="Times" ,size=6) -> plot2
 
 cowplot::plot_grid(plot1, plot2,  align = "vh", nrow = 1, ncol=2)
-ggsave("figs/scatterplot__scale_length.png", dpi = 500, height = 4, width = 8, units = "in")
+ggsave("figs/scatterplot__scale_length.tiff", dpi = 500, height = 4, width = 8, units = "in")
 
 
 merge %>%
